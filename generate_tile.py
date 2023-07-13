35fd5b85ae3e509ec2a808deed5c12653f6c011e
WoutCherlet <Wout.Cherlet@UGent.be>
2023-07-13 15:36:06 +0200

    read cuttable and non cuttable tiles

--- generate_tile.py
+++ generate_tile.py
@@ -15,8 +15,6 @@ from scipy.spatial import ConvexHull
 from perlin_numpy import generate_fractal_noise_2d
 from scipy import interpolate
 
-from preprocess_terrain import overlay_terrain
-
 
 DEBUG = False
 DEBUG_ALPHA_SHAPES = False
@@ -32,6 +30,7 @@ SEMANTIC_MAP = {
 
 POINTS_PER_METER = 10 # NOTE: if changing this, will probably need to recalibrate a lot of the other parameters too
 GRID_SIZE = 1
+STEP_SIZE = 0.01 # for binning in lowest point extraction and overlaying
 TREES_PER_PLOT = 1 # NOTE: slight variations in this to get different amount of instances per plot?
 # max size of plot
 MAX_SIZE_X = 60
@@ -69,12 +68,17 @@ def read_trees(mesh_dir, pc_dir, alpha=None):
     return trees
 
 def read_terrain_tiles(tile_dir):
-    terrain_tiles = []
-    for file in sorted(glob.glob(os.path.join(tile_dir, "*.ply"))):
+    terrain_tiles_cuttable = []
+    terrain_tiles_non_cuttable = []
+    for file in sorted(glob.glob(os.path.join(tile_dir, "cuttable", "*.ply"))):
         pc = o3d.io.read_point_cloud(file)
-        terrain_tiles.append(pc)
+        terrain_tiles_cuttable.append(pc)
+        terrain_tiles_non_cuttable.append(pc) # all cuttable tiles can also be used in non_cuttable situations
+    for file in sorted(glob.glob(os.path.join(tile_dir, "non_cuttable", "*.ply"))):
+        pc = o3d.io.read_point_cloud(file)
+        terrain_tiles_non_cuttable.append(pc)
     
-    return terrain_tiles
+    return [terrain_tiles_cuttable, terrain_tiles_non_cuttable]
 
 
 def get_initial_tree_position(tree_mesh, pointcloud, noise_2d, max_x_row, max_y_plot):
@@ -468,6 +472,270 @@ def trunk_height_influence_map_convex_circle(min_x, min_y, nx, ny, points_per_me
 
     return influence_map, height_map
 
+
+
+def extract_lowest(terrain_tile, step_size=STEP_SIZE):
+    # divide tile into bins of step_size and select bottom points in each bin by comparing to neighbouring bins
+
+    min_bound = terrain_tile.get_min_bound()
+    max_bound = terrain_tile.get_max_bound()
+
+    x_range = np.arange(min_bound[0], max_bound[0], step=step_size)
+    y_range = np.arange(min_bound[1], max_bound[1], step=step_size)
+
+    # bottom_cloud = o3d.geometry.PointCloud()
+    # top_cloud = o3d.geometry.PointCloud()
+
+    x_bins = len(x_range)
+    y_bins = len(y_range)
+
+    lowest = np.ones((x_bins, y_bins, 3))*np.inf
+    points_arr = np.asarray(terrain_tile.points)
+
+    bins = [[[] for y in range(y_bins)] for x in range(x_bins)]
+
+    for point in points_arr:
+        bin_idx = (point - min_bound) // step_size
+        idx_x = int(bin_idx[0])
+        idx_y = int(bin_idx[1])
+        bins[idx_x][idx_y].append(point)
+        if point[2] < lowest[idx_x][idx_y][2]:
+            lowest[idx_x][idx_y] = point
+        
+    points_bot = []
+    points_top = []
+    bins_lowest = [[None for y in range(y_bins)] for x in range(x_bins)]
+    for point in points_arr:
+        bin_idx = (point - min_bound) // step_size
+        i = int(bin_idx[0])
+        j = int(bin_idx[1])
+        i_min = max(0, i-1)
+        j_min = max(0, j-1)
+        i_max = min(i+1, x_bins-1)
+        j_max = min(j+1, y_bins-1)
+        remove = False
+        for x in range(i_min, i_max+1):
+            for y in range(j_min, j_max+1):
+                diff = point - lowest[x][y]
+                dist2d = diff[0]*diff[0] + diff[1]*diff[1]
+                if diff[2] > 0 and diff[2]*diff[2] > dist2d:
+                    remove = True
+                    break
+            if remove:
+                break
+        if remove:
+            points_top.append(point)
+        else:
+            points_bot.append(point)
+            bins_lowest[i][j] = point
+    
+    # bottom_cloud = o3d.geometry.PointCloud()
+    # bottom_cloud.points = o3d.utility.Vector3dVector(np.array(points_bot))
+    # bottom_cloud.paint_uniform_color(np.array([0,1,0]))
+    # top_cloud = o3d.geometry.PointCloud()
+    # top_cloud.points = o3d.utility.Vector3dVector(np.array(points_top))
+    # top_cloud.paint_uniform_color(np.array([1,0,0]))
+    return bins, bins_lowest
+
+def precalc_lowest(terrain_tiles):
+    bin_arr = []
+    for tile in terrain_tiles:
+        bins_tuple = extract_lowest(tile)
+        bin_arr.append(bins_tuple)
+
+    return bin_arr
+
+def trunk_in_tile(cur_noise_tile):
+    # TODO: implement: decide if using bbox, tree hull, or center position
+    # plan: get max bound and min bounds of cur_noise_tile
+    # check with all (hulls or positions ??) if they are in tile
+    # if any tree in tile return True
+    return False
+
+def get_closest_lowest(bins_lowest, i, j, point):
+    # if no lowest point at i,j, look in tiles around it for a lowest point by checking moore neighboorhoud for closest lowest point
+    x_bins = len(bins_lowest)
+    y_bins = len(bins_lowest[0])
+
+    index_offset = 1
+    found = False
+    while not found:
+        i_min = max(0, i-index_offset)
+        j_min = max(0, j-index_offset)
+        i_max = min(i+index_offset, x_bins-1)
+        j_max = min(j+index_offset, y_bins-1)
+        closest_dist = np.inf
+        closest_xy = None
+        for x in (i_min, i_max):
+            for y in (j_min, j_max):
+                if bins_lowest[x][y] is not None:
+                    dist2d = np.linalg.norm(point - bins_lowest[x][y][:2])
+                    found = True
+                    if dist2d < closest_dist:
+                        closest_dist = dist2d
+                        closest_xy = (x,y)
+        if found:
+            return bins_lowest[closest_xy[0]][closest_xy[1]]
+        index_offset += 1
+
+def overlay_single_tile(terrain_tile, noise_tile, interpolator, bins_tuple):
+    # bins, bins_lowest = extract_lowest_alt(terrain_tile, step_size=STEP_SIZE)
+    bins, bins_lowest = bins_tuple
+
+    all_points = []
+
+    # move terrain tile to noise 
+    min_bounds_noise = np.min(noise_tile, axis=0)
+    terrain_tile = terrain_tile.translate(min_bounds_noise-terrain_tile.get_min_bound())
+
+    max_bounds_noise = np.max(noise_tile, axis=0)
+    extent_noise = max_bounds_noise - min_bounds_noise
+
+    max_num_bins_x = m.floor((1/STEP_SIZE)*extent_noise[0])
+    max_num_bins_y = m.floor((1/STEP_SIZE)*extent_noise[1])
+
+    # for all bins within noise tile
+    for i in range(max_num_bins_x):
+        for j in range(max_num_bins_y):
+            if len(bins[i][j]) == 0:
+                continue
+
+            bin_center_x = i*STEP_SIZE+STEP_SIZE/2 + min_bounds_noise[0]
+            bin_center_y = j*STEP_SIZE+STEP_SIZE/2 + min_bounds_noise[1]
+
+            center_arr = np.array([bin_center_x, bin_center_y])
+
+            # get closest lowest point
+            if bins_lowest[i][j] is None:
+                lowest_point = np.array(get_closest_lowest(bins_lowest, i, j, center_arr))
+            else:
+                lowest_point = bins_lowest[i][j]
+
+            # correct height of all points in tile to interpolated perlin noise height
+            # we estimate the height of the terrain tile as the height of the closest lowest point
+            height = interpolator((bin_center_x, bin_center_y))
+            height_correction = height - lowest_point[2]
+
+            points = np.array(bins[i][j])
+            corrected_points = points + np.array([0, 0, height_correction])
+            all_points.append(corrected_points)
+            
+    corrected_cloud = o3d.geometry.PointCloud()
+    corrected_cloud.points = o3d.utility.Vector3dVector(np.vstack(all_points))
+    # corrected_cloud.paint_uniform_color(np.array([0,1,0]))
+
+    noise_cloud = o3d.geometry.PointCloud()
+    noise_cloud.points = o3d.utility.Vector3dVector(np.array(noise_tile))
+    noise_cloud.paint_uniform_color(np.array([1,0,0]))
+
+    # o3d.visualization.draw_geometries([noise_cloud, corrected_cloud])
+
+    return noise_cloud, corrected_cloud
+
+def overlay_terrain(noise_2D, noise_coordinates, interpolator, terrain_tiles):
+
+    # get dimensions of noise terrain to fill up
+    pptile = GRID_SIZE*POINTS_PER_METER
+    n_x = len(noise_2D)
+    n_y = len(noise_2D[1])
+    n_tiles_x = m.ceil((n_x-1) / pptile)
+    x_edge_points_rng = (n_x-1) % pptile
+    n_tiles_y = m.ceil((n_y-1) / pptile)
+    y_edge_points_rng = (n_x-1) % pptile
+
+    cuttable_tiles = terrain_tiles[0]
+    non_cuttable_tiles = terrain_tiles[1]
+    random.shuffle(cuttable_tiles)
+    random.shuffle(non_cuttable_tiles)
+    cuttable_idx = 0
+    non_cuttable_idx = 0
+    n_cuttable = len(cuttable_tiles)
+    n_non_cuttable = len(non_cuttable_tiles)
+
+    # pre extract binned tile and lowest points
+    bins_cuttable = precalc_lowest(cuttable_tiles)
+    bins_non_cuttable = precalc_lowest(non_cuttable_tiles)
+
+    
+    print(f"Overlaying noise, {n_tiles_x*n_tiles_y} tiles of size {GRID_SIZE}x{GRID_SIZE}")
+    print(f"Selecting from library of {n_cuttable} cuttable tiles and {n_non_cuttable} noncuttable tiles")
+
+    merged_terrain_cloud = None
+
+    tiles = []
+    for i in range(n_tiles_x):
+        for j in range(n_tiles_y):
+            # slice coordinate list
+            cur_noise_tile = []
+
+            # need to get 2D tile from list of points, do some indexing magic based on the meshgrid format
+            x_index_range = pptile + 1
+            y_index_range = pptile + 1
+            # handle edge cases, little ugly but oh well
+            if i == (n_tiles_x-1) and x_edge_points_rng != 0:
+                x_index_range = x_edge_points_rng + 1
+            if j == (n_tiles_y - 1) and y_edge_points_rng != 0:
+                y_index_range = y_edge_points_rng + 1
+            
+            # shift in array of points, x is row index, y is column index
+            shift = i*pptile+j*n_x*pptile
+            for l in range(y_index_range):
+                cur_noise_tile.append(noise_coordinates[shift+l*n_x:shift+l*n_x+x_index_range])
+            cur_noise_tile = np.vstack(cur_noise_tile)
+
+            # TODO: randomly rotate tile? also need to rotate bins array
+
+            if trunk_in_tile(terrain_tiles):
+                cur_terrain_tile = cuttable_tiles[cuttable_idx]
+                bins_tuple = bins_cuttable[cuttable_idx]
+                cuttable_idx += 1
+                cuttable_idx = cuttable_idx % n_cuttable
+                if cuttable_idx == 0:
+                    # reshuffle tiles and bins in same order
+                    c = list(zip(cuttable_tiles, bins_cuttable))
+                    random.shuffle(c)
+                    cuttable_tiles, bins_cuttable = zip(*c)
+
+            else:
+                cur_terrain_tile = non_cuttable_tiles[non_cuttable_idx]
+                bins_tuple = bins_non_cuttable[non_cuttable_idx]
+                non_cuttable_idx += 1
+                non_cuttable_idx = non_cuttable_idx % n_non_cuttable
+                if non_cuttable_idx == 0:
+                    # reshuffle tiles and bins in same order
+                    c = list(zip(non_cuttable_tiles, bins_non_cuttable))
+                    random.shuffle(c)
+                    non_cuttable_tiles, bins_non_cuttable = zip(*c)
+            
+            # overlay single noise tile on noise
+            noise_cloud, tile_cloud = overlay_single_tile(cur_terrain_tile, cur_noise_tile, interpolator, bins_tuple)
+            tiles.append(tile_cloud)
+            tiles.append(noise_cloud)
+
+            if merged_terrain_cloud is None:
+                # copy constructor
+                merged_terrain_cloud = o3d.geometry.PointCloud(tile_cloud)
+            else:
+                merged_terrain_cloud += tile_cloud
+            # vis.add_geometry(noise_cloud)
+            # vis.add_geometry(tile_cloud)
+            
+            # vis.poll_events()
+            # vis.update_renderer()
+            # time.sleep(1)
+
+
+    # time.sleep(100)
+    # vis.destroy_window()
+
+    # tiles.append(full_noise_cloud)
+    # o3d.visualization.draw_geometries(tiles)
+
+    return merged_terrain_cloud
+    
+
+
+
 def points_in_alphashape(points, alphashape):
     mask = []
     for point in points:
@@ -652,7 +920,6 @@ def generate_tiles(mesh_dir, pc_dir, tiles_dir, out_dir, alpha=None, n_tiles=10)
     # read trees
     trees = read_trees(mesh_dir, pc_dir, alpha=alpha)
 
-    # TODO: expand to cuttable and non_cuttable
     terrain_tiles = read_terrain_tiles(tiles_dir)
 
     print(f"Read {len(trees)} trees")
@@ -685,8 +952,8 @@ def main():
         print(f"Couldn't read trees input dir {args.pointcloud_directory}!")
         return
     
-    if not os.path.exists(args.tile_directory):
-        print(f"Couldn't read tile input dir {args.tile_directory}!")
+    if not os.path.exists(os.path.join(args.tile_directory, "cuttable")) or not os.path.exists(os.path.join(args.tile_directory, "non_cuttable")):
+        print(f"Couldn't read cuttable or non_cuttable tile input dirs in {args.tile_directory}!")
         return
 
     
